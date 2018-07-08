a5237790699708caa810ff01c30bb98a296178b9
b-sonter <40931305+b-sonter@users.noreply.github.com>
2018-07-08 17:30:50 +1000

    initial code

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+/*
+
+Week 2 - 3813ICT Software Frameworks
+Student: Brianna Sonter
+Number: s2930629
+
+Workshop 2
+
+*/
+
+//create a new node project that prints out "Hello World"
+console.log("Hello World")
