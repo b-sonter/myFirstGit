609314edc21df6036b128c79a0629d0a60af7fc1
b-sonter <40931305+b-sonter@users.noreply.github.com>
2018-07-20 12:36:14 +1000

    Add Adding Function
    For workshop 2 - add a branch and add an adding function

--- helloworld.js
+++ helloworld.js
@@ -1,12 +1,24 @@
 /*
 
-Week 2 - 3813ICT Software Frameworks
+3813ICT Software Frameworks
 Student: Brianna Sonter
 Number: s2930629
 
-Workshop 2
+Workshop 1
 
 */
 
 //create a new node project that prints out "Hello World"
 console.log("Hello World")
+
+/*
+Workshop 2 - branching
+*/
+
+//addition of 5 and 10
+function addnums(num1, num2){
+  var result = numb1 + num2;
+  return result;
+}
+
+console.log(addnums(5, 10));
